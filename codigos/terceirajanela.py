ac57418f618ecf569ca7ff81ebcc32904ba1bd26
Dirceu Silvestre <dirceu_silvestre@hotmail.com>
2022-05-05 23:22:56 -0300

    quase pronto, já em fase de teste

--- codigos/terceirajanela.py
+++ codigos/terceirajanela.py
@@ -7,7 +7,7 @@ def janela3(nome_categoria):
 
     # Exibir A Lista #
 
-    lista_exibir = Listbox(janela3, font=('Comic Sans', 20))
+    lista_exibir = Listbox(janela3, font=('Comic Sans', 20), selectmode=SINGLE)
     arquivo_categorias = open(nome_categoria, "a")
     arquivo_categorias.close()
     arquivo_categorias = open(nome_categoria, "r")
@@ -17,10 +17,16 @@ def janela3(nome_categoria):
         lista_exibir.insert(END, item)
     lista_exibir.place(height= 200, x = 100, y = 150)
 
+    # Caixa de Texto #
+
+    nome_obra = Entry(janela3, bd=2, font=("Calibri", 15), justify=CENTER)
+    nome_obra.place(width=300, height=40, x=100, y=460)
+
     # Funções #
 
     def exibir_atualizacao():
         global lista_exibir
+        lista_exibir = Listbox(janela3, font=('Comic Sans', 20), selectmode=SINGLE)
         arquivo_categorias = open(nome_categoria, "a")
         arquivo_categorias.close()
         arquivo_categorias = open(nome_categoria, "r")
@@ -31,7 +37,7 @@ def janela3(nome_categoria):
         lista_exibir.place(height= 200, x = 100, y = 150)
 
     def incluir_obra():
-        nome = str(nome_categoria.get())
+        nome = str(nome_obra.get())
         lista_exibir.insert(END, nome)
         arquivo_categorias = open(nome_categoria, "a")
         arquivo_categorias.write(nome + ' \n')
@@ -39,10 +45,38 @@ def janela3(nome_categoria):
         exibir_atualizacao()
 
     def excluir_obra():
-        pass
+        nome = str(nome_obra.get())
+        arquivo_categorias = open(nome_categoria, "r")
+        arquivo_lido = arquivo_categorias.readlines()
+        arquivo_categorias.close()
+        lista_conteudo = [s for s in arquivo_lido if nome[0:(len(nome)//2)] in s]
+        if lista_conteudo != []:
+            local = arquivo_lido.index(lista_conteudo[0])
+            arquivo_lido.remove(lista_conteudo[0])
+            arquivo_categorias = open(nome_categoria, "w")
+            arquivo_categorias.writelines(arquivo_lido)
+            arquivo_categorias.close()
+        exibir_atualizacao()
 
     def alterar_obra():
-        pass
+        nome = str(nome_obra.get())
+        arquivo_categorias = open(nome_categoria, "r")
+        arquivo_lido = arquivo_categorias.readlines()
+        arquivo_categorias.close()
+        lista_conteudo = [s for s in arquivo_lido if nome[0:(len(nome)//2)] in s]
+        if lista_conteudo != []:
+            local = arquivo_lido.index(lista_conteudo[0])
+            arquivo_lido.remove(lista_conteudo[0])
+            arquivo_lido.insert(local, str(nome + ' \n'))
+            arquivo_categorias = open(nome_categoria, "w")
+            arquivo_categorias.writelines(arquivo_lido)
+            arquivo_categorias.close()
+        exibir_atualizacao()
+
+    def pegar_obra():
+        nome = lista_exibir.get(ACTIVE)
+        nome_obra.insert(0, nome[0:-2])
+        exibir_atualizacao()
 
     # Labels #
 
@@ -50,6 +84,9 @@ def janela3(nome_categoria):
     lab_secundaria.pack(side='top')
 
     # Botões #
+
+    botao_pegar = Button(janela3, text='PEGAR', command=pegar_obra)
+    botao_pegar.place(x=230, y=415)
 
     botao_incluir = Button(janela3, text='INCLUIR', command=incluir_obra)
     botao_incluir.place(x=100, y=520)
